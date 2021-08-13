3e22c49758023762ae1bb4aa637bd08e01e52195
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2021-08-13 16:27:04 +0900

    Fix mypy warning

--- tests/test_process_terminator.py
+++ tests/test_process_terminator.py
@@ -1,7 +1,7 @@
 """Test for process_terminator.py."""
 from typing import Callable
 
-from asynccpu.subprocess import terminate_processes
+from asynccpu.process_terminator import terminate_processes
 from tests.testlibraries.process_family import ProcessFamily
 
 
@@ -28,5 +28,6 @@ class TestTerminateProcess:
     def execute_test_terminate_processes(cls, callable_execute_terminate_processes: Callable[[int], None]) -> None:
         """Executes test for terminate_process()."""
         process_family = ProcessFamily()
+        assert process_family.child_process.pid
         callable_execute_terminate_processes(process_family.child_process.pid)
         process_family.assert_that_descendant_processes_are_terminated()
