8b26208f63e1b00190cf85632c5339916c74eb9b
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-10-27 15:15:21 +0900

    Bump version: 1.2.2 → 1.3.0

--- asynccpu/__init__.py
+++ asynccpu/__init__.py
@@ -6,7 +6,7 @@ from asynccpu.process_task_pool_executor import *  # noqa: F403
 
 __author__ = """Yukihiko Shinoda"""
 __email__ = "yuk.hik.future@gmail.com"
-__version__ = "1.2.2"
+__version__ = "1.3.0"
 
 __all__: list[str] = []
 __all__ += process_task_pool_executor.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
